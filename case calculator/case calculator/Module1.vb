07af3ca4e3cd0d17e4931c62252bffc4f77634b6
GhislainChe <ghislainche2007@gmail.com>
2025-05-13 07:40:28 +0100

    case calculator

---
+++ case calculator/case calculator/Module1.vb
@@ -0,0 +1,41 @@
+﻿Module Module1
+    Sub Main()
+        Dim num1, num2, result As Double
+        Dim operation As String
+
+        Console.Write("Enter first number: ")
+        num1 = Double.Parse(Console.ReadLine())
+
+        Console.Write("Enter second number: ")
+        num2 = Double.Parse(Console.ReadLine())
+
+        Console.Write("Enter operation (+, -, *, /): ")
+        operation = Console.ReadLine()
+
+        Select Case operation
+            Case "+"
+                result = num1 + num2
+            Case "-"
+                result = num1 - num2
+            Case "*"
+                result = num1 * num2
+            Case "/"
+                If num2 <> 0 Then
+                    result = num1 / num2
+                Else
+                    Console.WriteLine("Error: Division by zero.")
+                    Exit Sub
+                End If
+            Case Else
+                Console.WriteLine("Invalid operation.")
+                Exit Sub
+        End Select
+
+        Console.WriteLine("Result: " & result)
+
+        Console.WriteLine("Press Enter to exit...")
+        Console.ReadLine()
+    End Sub
+End Module
+
+
